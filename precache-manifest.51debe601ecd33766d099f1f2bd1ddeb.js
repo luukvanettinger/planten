85e0e5d3fbc809215f27ebbef27c180b62de2229
Luuk van Ettinger <luukvanettinger@Luuks-MacBook-Pro.local>
2020-08-23 12:42:06 +0200

    Updates

---
+++ precache-manifest.51debe601ecd33766d099f1f2bd1ddeb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5566a6cb5af7367edc13c1bb2e0af068",
+    "url": "/planten/index.html"
+  },
+  {
+    "revision": "229651d7e1a5827ff7cd",
+    "url": "/planten/static/css/main.75c7502f.chunk.css"
+  },
+  {
+    "revision": "0094a9468ac1188ac107",
+    "url": "/planten/static/js/2.5f84d2d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/planten/static/js/2.5f84d2d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "229651d7e1a5827ff7cd",
+    "url": "/planten/static/js/main.e6465c96.chunk.js"
+  },
+  {
+    "revision": "157d596dea5b0bbaa2a1",
+    "url": "/planten/static/js/runtime-main.832b2cf0.js"
+  },
+  {
+    "revision": "3f2720c521f925c094b80f63c873bb7a",
+    "url": "/planten/static/media/HelveticaNowText-Medium.3f2720c5.ttf"
+  }
+]);
